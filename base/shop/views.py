2c86d84eb0c8ce214616641cbe53caa22d0138e8
Soj044 <olegmuzcinin@gmail.com>
2025-03-24 14:04:21 +0300

    added migrations and some views + bootstrap library

--- base/shop/views.py
+++ base/shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import *
 
 def index(request):
-    return HttpResponse('Hello from shop')
+    courses = Course.objects.all()
+    return render(request, 'shop/index.html', {'courses': courses})
